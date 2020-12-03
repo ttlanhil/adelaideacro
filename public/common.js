2121595bba287b661b01340b8bb310a508d674bd
Adam <ttlanhil@gmail.com>
2020-12-03 11:20:09 +1030

    embed Google maps onload, rather than onclick. fallback remains a link to google maps

--- public/common.js
+++ public/common.js
@@ -42,10 +42,19 @@ function build_accordions() {
 
 
 window.onload = function() {
+    // add needed code/styles for the accordions
     build_accordions();
+
+    // remove "display:initial" from homepage, then jump to homepage <section>
+    // allows page to still work reasonably well with JS disabled
     document.getElementById("home").removeAttribute("style");
     if ( ! document.location.hash ) {
         document.location.hash = "#home";
     }
+
+    // add havejs class to body, used for some of the CSS
     document.body.classList.add("havejs");
+
+    // embed map into location tab
+    document.getElementById("map-wrapper").innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.0998022008152!2d138.60451631564402!3d-34.92903388037659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ab0ced3a9f810f1%3A0xee378ea10f89cdec!2sPoint%20A!5e0!3m2!1sen!2sau!4v1603861345027!5m2!1sen!2sau" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
 }
