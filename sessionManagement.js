66b2ee50a2219f206e7b95e6c18d22119209caf2
Adam <ttlanhil@gmail.com>
2021-01-09 05:06:37 +1030

    call format for some of the functions has changed

--- sessionManagement.js
+++ sessionManagement.js
@@ -3,22 +3,30 @@ const userSessions = require("./admin/user_sessions.js");
 // TODO: add a UI of some sort
 
 // TODO: ability to prune old user accounts (particularly anonymous logins)
+// TODO: remove DB entries for any deleted user accounts
+
+// remove old sessions
+// TODO: remove user tokens for expired or deleted sessions
+
+const testSessionID = "-MPscMFlTngGQt_CyvqJ";
+const stretchSessionID = "-MQ5ymANs7HAbkVusdEr";
 
 
 // userSessions.setModerator("zOS2BL42Osb8ojwaZXRfyys9Juq1");
 // userSessions.setParticipant(["qvFgAQhQQ5U3leY2YRX6L4YfR513", "3gkpYXa8k2daQVe63k0YTfGjl6E2"]);
 
 // userSessions.addSession("Stretch & Strengthen", Date.parse("2021-01-08 17:45 +10:30")/1000);
-// userSessions.updateSession("-MQ5ymANs7HAbkVusdEr", {start: Date.parse("2021-01-08 19:45 +10:30")/1000});
 
-const start = Date.parse("2021-01-05 23:00 +10:30")/1000;
-const end = start + 24*60*60;
-userSessions.updateSession("-MPscMFlTngGQt_CyvqJ", {start, end});
+// userSessions.updateSession(stretchSessionID, {start: Date.parse("2021-01-08 19:45 +10:30")/1000});
 
-userSessions.setModerator("qvFgAQhQQ5U3leY2YRX6L4YfR513", false);
+// const start = Date.parse("2021-01-08 23:00 +10:30")/1000;
+// const end = start + 24*60*60;
+// userSessions.updateSession(testSessionID, {start, end});
 
-userSessions.listSessions();
+// userSessions.setModerator("qvFgAQhQQ5U3leY2YRX6L4YfR513", false);
 
-userSessions.listUsers();
+userSessions.listSessions().then(console.log);
 
-// userSessions.getUserData("J8PCPeWW3wZOKKDfcNRQuo8WrDS2").then(console.log);
+userSessions.listUsers().then(console.log);
+
+// userSessions.getUserData("Tp6WPruKcudEJwKgEncOxqU2LNv1").then(console.log);
